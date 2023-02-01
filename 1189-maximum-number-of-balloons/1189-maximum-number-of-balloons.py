c3e05aa239f02208034096cf0f8daf2c753acbf0
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-02-01 21:31:20 +0600

    Time: 21 ms (99.58%), Space: 13.9 MB (45.22%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         from operator import countOf
-        a = set()
-        d = set()
         b = ['b','a','n']
         c = ['o','l']
-        for i in range(0, len(b)):
-            a.add(countOf(text, b[i]))
-        for i in range(0, len(c)):
-            d.add(countOf(text, c[i]))
+        a = [countOf(text, x) for x in b]
+        d = [countOf(text, x) for x in c]
         if 2*min(a)>min(d) and min(d)<2:
             return 0
         elif 2*min(a)>min(d):
