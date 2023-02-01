5db9850986b66bd84191f9bb77285238714df13b
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-02-01 22:06:57 +0600

    Time: 5384 ms (5.94%), Space: 16.3 MB (6.16%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -1,16 +1,10 @@
 class Solution:
     def countTriples(self, n: int) -> int:
         f=0
-        a = []
-        b = []
         c=[]
         for i in range(1, n+1):
-            a.append(i**2)
-            b.append(i**2)
-        for x in a:
-            for z in b:
-                c.append(x+z)
-        for x in a:
-            f = f + countOf(c, x)
+            for z in range(1, n+1):
+                c.append(i**2+z**2)
+        for i in range(1,n+1): 
+            f = f + countOf(c, i**2)
         return f
-            
