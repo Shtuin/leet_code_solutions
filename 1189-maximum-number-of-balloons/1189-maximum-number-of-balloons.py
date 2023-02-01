12cacff32a35d494c2b5ac537213d351ea570da2
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-02-01 21:25:22 +0600

    Time: 29 ms (93.63%), Space: 14 MB (45.22%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -5,10 +5,10 @@ class Solution:
         d = set()
         b = ['b','a','n']
         c = ['o','l']
-        for i in range(0, len(b)):
-            a.add(countOf(text, b[i]))
-        for i in range(0, len(c)):
-            d.add(countOf(text, c[i]))
+        for i in b:
+            a.add(countOf(text, i))
+        for i in c:
+            d.add(countOf(text, i))
         if 2*min(a)>min(d) and min(d)<2:
             return 0
         elif 2*min(a)>min(d):
