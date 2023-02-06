5854d5e2aad2c025194a100ad07a0e9f775c9572
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-02-06 15:09:19 +0600

    Time: 45 ms (28.48%), Space: 13.9 MB (19.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,23 +1,6 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        from operator import countOf   
-        d=0
-        f=[]
-        if (countOf(s, "(") == countOf(s, ")") and countOf(s, "[") == countOf(s, "]") and countOf(s, "{") == countOf(s, "}")):
-            for i in range(0,len(s)-1):
-                if (s[i] == "(" and s[i+1] =="]") or (s[i] == "(" and s[i+1] =="}") or (s[i] == "[" and s[i+1] ==")") or (s[i] == "[" and s[i+1] =="}") or (s[i] == "{" and s[i+1] =="]") or (s[i] == "{" and s[i+1] ==")"):
-                    d+=1
-                f.append(s[i])
-                if countOf(f, "(")< countOf(f, ")") or countOf(f, "{")< countOf(f, "}")  or countOf(f, "[")< countOf(f, "]"):
-                    d+=1    
-
-            if s == "[([]])":
-                return False
-                
-            if d>0:
-                return False
-            else:
-                return True        
-        else:
-            return False
+        while '()' in s or '[]'in s or '{}' in s:
+            s = s.replace('()','').replace('[]','').replace('{}','')
+        return False if len(s) !=0 else True
     
