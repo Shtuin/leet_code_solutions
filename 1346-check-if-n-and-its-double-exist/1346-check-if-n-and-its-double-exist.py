ec2b23ef9e456b58719463b0db0b83678295fbfd
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-02-07 22:04:13 +0600

    Time: 59 ms (67.12%), Space: 14 MB (11.35%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -1,11 +1,20 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        for i in range(0, len(arr)):
-            if arr[i]%2 ==0:
-                for j in arr:
-                    if divmod(arr[i], 2)[0] == j and j!=0:
-                        return True
-                    elif divmod(arr.count(0),2)[0]>0:
-                        return True
+        # for i in range(0, len(arr)):
+        #     if arr[i]%2 ==0:
+        #         for j in arr:
+        #             if divmod(arr[i], 2)[0] == j and j!=0:
+        #                 return True
+        #             elif divmod(arr.count(0),2)[0]>0:
+        #                 return True
+        # else:
+        #     return False
+        arr2 = [2*i for i in arr]
+        if len(set(arr2).intersection(set(arr)))>0 and set(arr2).intersection(set(arr)) !={0}:
+            return True
+        elif arr.count(0)>1:
+            return True
         else:
             return False
+         
+            
