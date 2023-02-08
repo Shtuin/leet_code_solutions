54fd0bff801a6267256cc01f8258820e1ff84fbc
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-02-08 10:31:01 +0600

    Time: 25 ms (93.63%), Space: 13.8 MB (66.24%) - LeetHub

--- 1185-day-of-the-week/1185-day-of-the-week.py
+++ 1185-day-of-the-week/1185-day-of-the-week.py
@@ -1,11 +1,9 @@
 class Solution:
     def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
-        s = [0,31,59,90,120,151,181,212,243,273,304,334]   
+        s = {1:0,2:31,3:59,4:90,5:120,6:151,7:181,8:212,9:243,10:273,11:304,12:334}   
         if month<3 and year%4==0 or year%100==0:
-            d = (year*365+int((year-1)/4)+s[month-1]+day+5)%7
-        # elif year%100==0:
-        #     d = (year*365+int((year)/4)+s[month-1]+day+4)%7            
+            d = (year*365+int((year-1)/4)+s[month]+day+5)%7
         else:    
-            d = (year*365+int(year/4)+s[month-1]+day+5)%7
-        weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-        return weekday[d-1]
+            d = (year*365+int(year/4)+s[month]+day+5)%7
+        weekday = {1:'Monday', 2:'Tuesday', 3:'Wednesday', 4:'Thursday', 5:'Friday', 6:'Saturday', 0:'Sunday'}
+        return weekday[d]
