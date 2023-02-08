69a8a05af84fbccca7e8d33ef956bc75620c9c50
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-02-08 23:26:33 +0600

    Time: 38 ms (52.23%), Space: 13.8 MB (50.08%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.py
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.py
@@ -1,10 +1,15 @@
 class Solution:
     def alternateDigitSum(self, n: int) -> int:
-        n_str = list(map(int, str(n))) 
-        c=[]
-        for i in range(len(n_str)):
-            if i%2 ==0:
-                c.append(n_str[i])
-            else:
-                c.append(-1*n_str[i])
-        return sum(c)
+        # n_str = list(map(int, str(n))) 
+        # c= 
+        # for i in range(len(n_str)):
+        #     if i%2 ==0:
+        #         c.append(n_str[i])
+        #     else:
+        #         c.append(-1*n_str[i])
+        # return sum(c)
+        result = 0
+        st = str(n)
+        for i in range(len(st)):
+            result = (result + int(st[i])) if i % 2 == 0 else (result - int(st[i]))
+        return result
