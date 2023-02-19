f0245af9e15c66a874b99a3b6827632ed1317f13
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-02-19 23:38:38 +0600

    Time: 52 ms (28.47%), Space: 13.9 MB (53.90%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -0,0 +1,10 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:               
+        d={}    
+        for z in range(1, len(nums)+1):
+            d[z] = sum(i>=z for i in nums)
+        for key,value in d.items():
+            if key==value:
+                return key
+        else:
+            return -1
