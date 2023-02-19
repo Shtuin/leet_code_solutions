212c8a1e7eee9428cc32f55f558ff962868377b1
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-02-19 21:22:39 +0600

    Time: 32 ms (70.42%), Space: 13.7 MB (92.02%) - LeetHub

--- 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.py
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.py
@@ -1,16 +1,3 @@
 class Solution:
     def distinctIntegers(self, n: int) -> int:
-        import numpy as np
-        c = [n]
-        x = np.arange(1,n)
-        i=0
-        while i in range(0,n+1):
-            for v in c:
-                d = len(c)
-                for z in x:
-                    if z!=0and  v%z ==1 and c.count(z) ==0:
-                        c.append(z)
-                if d+1 >= n:
-                    break
-            i+=1
-        return len(c)
+        return n if n<=1 else n-1
