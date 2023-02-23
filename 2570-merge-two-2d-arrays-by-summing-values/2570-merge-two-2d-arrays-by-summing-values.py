ad185b5d88441d97d5cb2bf37f12df294c395b44
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-02-23 14:10:56 +0600

    Time: 68 ms (43.69%), Space: 14 MB (97.76%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.py
@@ -3,22 +3,21 @@ class Solution:
         n1 = 0
         n2=0
         ans = []
-        while n1<len(nums1) or n2<len(nums2):
-            if n1 == len(nums1):
+        while n1<len(nums1) and n2<len(nums2):
+            if nums1[n1][0] == nums2[n2][0]:
+                ans.append([nums1[n1][0], nums1[n1][1]+nums2[n2][1]])
+                n1+=1
+                n2+=1
+            elif nums1[n1]>nums2[n2]:
                 ans.append(nums2[n2])
                 n2+=1
-            elif n2 ==len(nums2):
+            elif nums1[n1]<nums2[n2]:
                 ans.append(nums1[n1])
                 n1+=1
-            else:
-                if nums1[n1][0] == nums2[n2][0]:
-                    ans.append([nums1[n1][0], nums1[n1][1]+nums2[n2][1]])
-                    n1+=1
-                    n2+=1
-                elif nums1[n1]>nums2[n2]:
-                    ans.append(nums2[n2])
-                    n2+=1
-                else:
-                    ans.append(nums1[n1])
-                    n1+=1
+        while n1 == len(nums1) and n2< len(nums2):
+            ans.append(nums2[n2])
+            n2+=1
+        while n1 < len(nums1) and n2== len(nums2):
+            ans.append(nums1[n1]) 
+            n1+=1
         return ans
