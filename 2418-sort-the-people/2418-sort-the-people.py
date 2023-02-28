44064ab682644a627faacc341270ca22e0c65717
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-02-28 17:44:41 +0600

    Time: 127 ms (53.41%), Space: 14.5 MB (36.90%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        a = {}
+        i=0
+        j=0
+        while i < len(names) and j<len(heights):
+            a[heights[j]] = names[i]
+            i+=1
+            j+=1
+        return [i[1] for i in sorted(a.items(), key = lambda x: x[0], reverse = True)]
+        
