34e0a96212bd04d2e0e715102bf3c7f440058cb4
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-03-24 13:55:36 +0600

    Time: 27 ms (91.82%), Space: 13.8 MB (96.65%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
@@ -1,8 +1,7 @@
 class Solution:
     def rearrangeCharacters(self, s: str, target: str) -> int:
-        tar = set([*target])
         min = len(s) 
-        for i in tar:
+        for i in set([*target]):
             if int(s.count(i)/target.count(i))<min:
                 min = int(s.count(i)/target.count(i))
         return min
