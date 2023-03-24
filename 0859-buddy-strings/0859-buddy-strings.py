6c451d1b44f060d8dfbfd17ef4061a636420f16a
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-03-24 15:13:04 +0600

    Time: 953 ms (5.06%), Space: 14.1 MB (52.83%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.py
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -2,13 +2,13 @@ class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
         i=0
         res=0
-        while i<len(s):
-            if s.count(s[i])!=goal.count(s[i]) or len(s)!=len(goal):
-                return False
-            else:
-                if s==goal and len(set([*s]))!=len(s):
-                    return True
-                elif s==goal and len(set([*s]))==len(s):
+        if s==goal and len(set([*s]))!=len(s):
+            return True
+        elif s==goal and len(set([*s]))==len(s):
+            return False
+        else:
+            while i<len(s):
+                if s.count(s[i])!=goal.count(s[i]) or len(s)!=len(goal):
                     return False
                 elif s[i]!=goal[i]:
                     res+=1
