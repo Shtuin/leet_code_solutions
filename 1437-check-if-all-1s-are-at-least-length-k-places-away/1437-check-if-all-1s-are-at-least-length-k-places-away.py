46e63216809974b3ae4d1cb41ee17cd9077c8d65
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-07-08 18:31:42 +0600

    Time: 557 ms (45.76%), Space: 19.2 MB (90.23%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.py
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.py
@@ -1,7 +1,21 @@
 class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
-        a = []
-        for i in range(len(nums)):
-            if nums[i] == 1:
-                a.append(i+1)
-        return all((a[i+1]-a[i]) >= k+1 for i in range(len(a)-1))
+        # initialize the counter of zeros to k
+        # to pass the first 1 in nums
+        count = k
+        
+        for num in nums:
+            # if the current integer is 1
+            if num == 1:
+                # check that number of zeros in-between 1s
+                # is greater than or equal to k
+                if count < k:
+                    return False
+                # reinitialize counter
+                count = 0
+            # if the current integer is 0
+            else:
+                # increase the counter
+                count += 1
+                
+        return True
