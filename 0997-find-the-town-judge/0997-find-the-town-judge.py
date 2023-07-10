68abd264f7288684c7d3f6c88b7ef155b8d99595
Shtuin <123573300+Shtuin@users.noreply.github.com>
2023-07-10 21:54:17 +0600

    Time: 3257 ms (5.15%), Space: 22 MB (54.20%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,32 +1,31 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        # if n ==1 and len(trust) == 0:
-        #     return 1
-        # else:
-        #     b = [i[1] for i in trust]
-        #     i=0
-        #     while i<=n:
-        #         if b.count(i) == n-1 and [i[0] for i in trust].count(i) == 0:
-        #             return i
-        #         else:
-        #             i+=1
-        #     return -1
-        if n==1:
-            return n
-        d={}
-        k={}
-        for i in range(1,n+1):
-            d[i]=1
-        for i in trust:
-            if i[0] in d:
-                del d[i[0]]
-            if i[1] in k:
-                k[i[1]]=k[i[1]]+1
-            else:
-                k[i[1]]=1
-        if len(d)!=1:
+        if n ==1 and len(trust) == 0:
+            return 1
+        else:
+            i=0
+            while i<=n:
+                if [i[1] for i in trust].count(i) == n-1 and [i[0] for i in trust].count(i) == 0:
+                    return i
+                else:
+                    i+=1
             return -1
-        for i,j in k.items():
-            if j==n-1:
-                return i
-        return -1
+        # if n==1:
+        #     return n
+        # d={}
+        # k={}
+        # for i in range(1,n+1):
+        #     d[i]=1
+        # for i in trust:
+        #     if i[0] in d:
+        #         del d[i[0]]
+        #     if i[1] in k:
+        #         k[i[1]]=k[i[1]]+1
+        #     else:
+        #         k[i[1]]=1
+        # if len(d)!=1:
+        #     return -1
+        # for i,j in k.items():
+        #     if j==n-1:
+        #         return i
+        # return -1
